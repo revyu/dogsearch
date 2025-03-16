3d616a38a2cfd8ac808da62f9f79322dac358cde
vits0 <a.bogdanov.2022@stud.nstu.ru>
2025-03-17 05:21:24 +0700

    Автообновление данных

---
+++ parser/geolocation/long_lat_converter.py
@@ -0,0 +1,31 @@
+import requests
+
+VK_ACCESS_TOKEN = "your_access_token"  # 🔹 Вставьте ваш токен
+API_VERSION = "5.131"
+
+
+def get_lat_lon_vk(address):
+    url = "https://api.vk.com/method/maps.search"
+    params = {
+        "q": address,
+        "v": API_VERSION,
+        "access_token": VK_ACCESS_TOKEN
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    try:
+        # Достаем первую найденную точку
+        place = data["response"]["places"]["items"][0]
+        lat, lon = place["latitude"], place["longitude"]
+        return lat, lon
+    except (KeyError, IndexError):
+        return None, None
+
+
+# Тест запроса
+address = "Москва, Красная площадь, 1"
+latitude, longitude = get_lat_lon_vk(address)
+
+print(f"Широта: {latitude}, Долгота: {longitude}")
