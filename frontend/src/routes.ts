373eae849de2ad5bcce5a58b0bdffae473cbb5f1
revyu <revyakin8800@gmail.com>
2025-04-27 22:28:09 +0700

    added routes for animal card page

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -12,14 +12,13 @@ export const DEFAULT_VIEW = 'default_view';
 
 export const DEFAULT_VIEW_PANELS = {
   HOME: 'home',
-  PERSIK: 'persik',
+  ANIMAL: 'animal', 
 } as const;
 
 export const routes = RoutesConfig.create([
   createRoot(DEFAULT_ROOT, [
     createView(DEFAULT_VIEW, [
       createPanel(DEFAULT_VIEW_PANELS.HOME, '/', []),
-      createPanel(DEFAULT_VIEW_PANELS.PERSIK, `/${DEFAULT_VIEW_PANELS.PERSIK}`, []),
     ]),
   ]),
 ]);
